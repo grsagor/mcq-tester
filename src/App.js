685b763c560d972b3e48eb2caea394c107975157
grsagor <grsagor08@gmail.com>
2022-10-11 01:22:56 +0600

    initially created routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <div>This is home</div>
+    },
+    {
+      path: 'topics',
+      element: <div>This is topics</div>
+    },
+    {
+      path: 'statistics',
+      element: <div>This is statistics</div>
+    },
+    {
+      path: 'blogs',
+      element: <div>This is blogs</div>
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
